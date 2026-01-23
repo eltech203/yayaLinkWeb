7869409159b091028c177f6cc12b23fe6aa5be53
eltech203 <eltech203@gmail.com>
2026-01-23 04:58:36 +0300

    SD

--- pages/selection.vue
+++ pages/selection.vue
@@ -17,7 +17,7 @@
                                 <div style="padding: 0px;" class="">
                                     <v-autocomplete v-model="filters.county" clearable filled rounded dense :loading="loading" @change="fetchCandidates" :items="counties" :search-input.sync="search" cache-items class="mx-2" flat hide-no-data hide-details placeholder="Search county...   "></v-autocomplete>
 
-                                        <v-btn large elevation="0"  @click="show = !show" style="border-radius: 12px;background-color: aliceblue; margin-right: 20px; padding-bottom: 0px;">
+                                        <v-btn large elevation="0"  @click="show = !show" style="border-radius: 12px;background-color: aliceblue; margin-right: 20px; padding-bottom: 0px; margin-top: 10px;">
                                             <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-tune' }}</v-icon>
                                             <p style="margin-top: 15px; font-size: 0.9rem; font-weight: 900;margin-right: 10px;">Filters</p>
                                         </v-btn>
